09b1350de324689adbdd3d2e6d9af1d9eeac3dd3
Goodness-Chukwudi <chukwudi.ibeche@moninetworks.com>
2022-10-31 21:40:34 +0100

    fixup! recreated task without p5js

--- src/components/controller/DirectionControls.jsx
+++ src/components/controller/DirectionControls.jsx
@@ -1,11 +1,5 @@
-import rightControl from "../../images/arrow right.png"
-import leftControl from "../../images/arrow left.png"
-import upControl from "../../images/arrow up.png"
-import downControl from "../../images/arrow down.png"
-
 
     function DirectionControls(props) {
-
 
 		return (
             // onMouseDown and onMouseUp events were used to include support for non-touch desktop browsers also
@@ -13,26 +7,26 @@ import downControl from "../../images/arrow down.png"
             <>
                 <div onTouchStart={() => props.moveHorizontal(true)} onTouchEnd={props.stopMovement}
                     onMouseDown={() => props.moveHorizontal(true)} onMouseUp={props.stopMovement}
-                    className="position-absolute top-50 end-0 translate-middle-y right">
-                    <img className="img-fluid" src={rightControl} alt="right"/>
+                    className="position-absolute top-50 end-0 translate-middle-y right arrow-right"
+                >
                 </div>
 
                 <div onTouchStart={() => props.moveHorizontal(false)} onTouchEnd={props.stopMovement}
                     onMouseDown={() => props.moveHorizontal(false)} onMouseUp={props.stopMovement}
-                    className="position-absolute top-50 start-0 translate-middle-y left">
-                    <img className="img-fluid" src={leftControl} alt="left"/>
+                    className="position-absolute top-50 start-0 translate-middle-y left arrow-left"
+                >
                 </div>
 
                 <div onTouchStart={() => props.moveVertical(false)} onTouchEnd={props.stopMovement}
                     onMouseDown={() => props.moveVertical(false)} onMouseUp={props.stopMovement}
-                    className="position-absolute top-0 start-50 translate-middle-x up">
-                    <img className="img-fluid" src={upControl} alt="up"/>
+                    className="position-absolute top-0 start-50 translate-middle-x up arrow-up"
+                >
                 </div>
 
                 <div onTouchStart={() => props.moveVertical(true)} onTouchEnd={props.stopMovement}
                     onMouseDown={() => props.moveVertical(true)} onMouseUp={props.stopMovement}
-                    className="position-absolute bottom-0 start-50 translate-middle-x down">
-                    <img className="img-fluid" src={downControl} alt="down"/>
+                    className="position-absolute bottom-0 start-50 translate-middle-x down arrow-down"
+                >
                 </div>
             </>
         );
