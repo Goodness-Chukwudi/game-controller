41ebdf258e84779ef60e017bca2de7543000b295
Goodness-Chukwudi <chukwudi.ibeche@moninetworks.com>
2022-10-31 20:15:51 +0100

    recreated task without p5js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Controller from './components/controller/Controller';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Controller />
+    </div>
+  );
+}
+
+export default App;
